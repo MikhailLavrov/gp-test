423b4c56b432754813ebc83b5c480cbd84ce3761
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-02-08 16:25:31 +0400

    add cancel handler to modal confirm, refactoring

--- src/components/ModalNotify.tsx
+++ src/components/ModalNotify.tsx
@@ -3,26 +3,37 @@ import React, { createContext } from 'react';
 import { Button, Modal, Space } from 'antd';
 
 const ReachableContext = createContext<string | null>(null);
-// const UnreachableContext = createContext<string | null>(null);
-
-const config = {
-  title: 'Вы уверены, что хотите аннулировать товар(ы): ',
-  content: (
-    <>
-      <ReachableContext.Consumer>{(name) => `${name} ?`}</ReachableContext.Consumer>
-      {/* <br />
-      <UnreachableContext.Consumer>{(name) => `Unreachable: ${name}!`}</UnreachableContext.Consumer> */}
-    </>
-  ),
-};
 
 const ModalNotify: React.FC = ({selectedDocuments}) => {
   const [modal, contextHolder] = Modal.useModal();
+  
+  let handleCancel = () => {
+    fetch('https://63e1288bdd7041cafb4281ad.mockapi.io/cancel', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ selectedDocuments })
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+
+  let onModalConfirm = () => {
+    modal.confirm({
+      title: 'Вы уверены, что хотите аннулировать товар(ы): ',
+      okText: 'Применить',
+      cancelText: 'Отклонить',
+      onOk: handleCancel,
+      content: <ReachableContext.Consumer>{(name) => `${name} ?`}</ReachableContext.Consumer>,
+    })
+  }
 
   return (
     <ReachableContext.Provider value={selectedDocuments.map(doc => doc.name).join(', ')}>
       <Space>
-        <Button onClick={ () => modal.confirm(config) } disabled={selectedDocuments.length === 0}>Аннулировать</Button>
+        <Button onClick={onModalConfirm} disabled={selectedDocuments.length === 0}>Аннулировать</Button>
       </Space>
       {/* `contextHolder` should always be placed under the context you want to access */}
       {contextHolder}
